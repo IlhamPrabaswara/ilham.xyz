9c057187cf046bc3ff90d16ac7bb735a63a2ca8b
ilhamprabaswara <ilhamprabaswara@gmail.com>
2021-07-21 12:22:55 +0700

    Add side navigation

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+function show() {
+    var toggle = document.querySelector('.slide-out-menu');
+    // var toggle = document.getElementsByClassName('slide-out-menu');
+    toggle.classList.toggle('slide-out-menu-show')
+}
