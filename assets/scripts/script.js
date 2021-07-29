95dc3e299ae94043c7ff4c5f43d44b81cdd9b8b4
IlhamPrabaswara <Ilhamprabaswara@gmail.com>
2021-07-30 01:16:46 +0700

    Delete assets directory

--- assets/scripts/script.js
+++
@@ -1,5 +0,0 @@
-function show() {
-    var toggle = document.querySelector('.slide-out-menu');
-    // var toggle = document.getElementsByClassName('slide-out-menu');
-    toggle.classList.toggle('slide-out-menu-show')
-}
